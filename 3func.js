02a9f1a880d78ff495bbd0c9b789abb463ed26f1
Rogier van Oudheusden <rogerthat@live.nl>
2021-12-14 15:50:48 +0100

    Add files via upload

---
+++ 3func.js
@@ -0,0 +1,29 @@
+// const calculation = function(number1, number2) {
+//     const square1 = number1 * number1;
+//     const square2 = number2 * number2;
+//     const total = square1 + square2;
+//     const result = total * total
+//     return result;
+// };
+
+// console.log(calculation(2, 3)); 
+
+// function calculation(number1, number2) {
+//     const square1 = number1 * number1;
+//     const square2 = number2 * number2;
+//     const total = square1 + square2;
+//     const result = total * total
+//     return result;
+// };
+
+// console.log(calculation(2, 3)); 
+
+const calculation = (number1, number2) => {
+    const square1 = number1 * number1;
+    const square2 = number2 * number2;
+    const total = square1 + square2;
+    const result = total * total
+    return result;
+};
+
+console.log(calculation(2, 3)); 
